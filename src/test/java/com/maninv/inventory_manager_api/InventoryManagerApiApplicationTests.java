130eea0892bb713153e64f7641585c618ab0d196
pedrin <pedrinho.top07@gmail.com>
2025-08-07 23:27:42 -0300

    initial commit

---
+++ src/test/java/com/maninv/inventory_manager_api/InventoryManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maninv.inventory_manager_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
